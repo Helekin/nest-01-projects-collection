3176a46cae14c81d1ac3a56557c64becf8dda4af
Helekin <clsjos2294@gmail.com>
2026-01-26 17:27:25 -0500

    Nest Intro

--- nest-02-car-dealership/src/cars/cars.module.ts
+++ nest-02-car-dealership/src/cars/cars.module.ts
@@ -1,19 +1,51 @@
-import { Get, Module, Param } from '@nestjs/common';
+import {
+  Body,
+  Delete,
+  Get,
+  Module,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CarsController } from './cars.controller';
+import { CarsService } from './cars.service';
 
 @Module({
   controllers: [CarsController],
+  providers: [CarsService],
 })
 export class CarsModule {
-  private cars = ['Toyota', 'Honda', 'Jeep'];
+  constructor(private readonly carsService: CarsService) {}
 
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
   getCarById(@Param('id') id: string) {
-    return this.cars[id];
+    return this.carsService.findOneById(+id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    return {
+      ok: true,
+      method: 'POST',
+    };
+  }
+
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: Number, @Body() body: any) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'delete',
+      id,
+    };
   }
 }
