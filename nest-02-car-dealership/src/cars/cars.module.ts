d02c1c9c9b64efbc704b27eebea405d2b66c634a
Helekin <clsjos2294@gmail.com>
2026-01-28 16:30:24 -0500

    Dtos and Validations

--- nest-02-car-dealership/src/cars/cars.module.ts
+++ nest-02-car-dealership/src/cars/cars.module.ts
@@ -5,47 +5,16 @@ import {
   Module,
   Param,
   ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
 import { CarsController } from './cars.controller';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Module({
   controllers: [CarsController],
   providers: [CarsService],
 })
-export class CarsModule {
-  constructor(private readonly carsService: CarsService) {}
-
-  @Get()
-  getAllCars() {
-    return this.carsService.findAll();
-  }
-
-  @Get(':id')
-  getCarById(@Param('id') id: string) {
-    return this.carsService.findOneById(+id);
-  }
-
-  @Post()
-  createCar(@Body() body: any) {
-    return {
-      ok: true,
-      method: 'POST',
-    };
-  }
-
-  @Patch(':id')
-  updateCar(@Param('id', ParseIntPipe) id: Number, @Body() body: any) {
-    return body;
-  }
-
-  @Delete(':id')
-  deleteCar(@Param('id', ParseIntPipe) id: number) {
-    return {
-      method: 'delete',
-      id,
-    };
-  }
-}
+export class CarsModule {}
---
+++ nest-02-car-dealership/src/cars/dto/create-car.dto.ts
@@ -0,0 +1,9 @@
+import { isString, IsString } from 'class-validator';
+
+export class CreateCarDto {
+  @IsString()
+  readonly brand: string;
+
+  @IsString()
+  readonly model: string;
+}
