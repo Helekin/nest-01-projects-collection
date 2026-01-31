b7bad755f8df5693048ae0f82f97f031a272d782
Helekin <clsjos2294@gmail.com>
2026-01-30 20:22:48 -0500

    Seed Data

--- nest-02-car-dealership/src/cars/cars.module.ts
+++ nest-02-car-dealership/src/cars/cars.module.ts
@@ -1,20 +1,11 @@
-import {
-  Body,
-  Delete,
-  Get,
-  Module,
-  Param,
-  ParseIntPipe,
-  ParseUUIDPipe,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
+
 import { CarsController } from './cars.controller';
 import { CarsService } from './cars.service';
-import { CreateCarDto } from './dto/create-car.dto';
 
 @Module({
   controllers: [CarsController],
   providers: [CarsService],
+  exports: [CarsService],
 })
 export class CarsModule {}
