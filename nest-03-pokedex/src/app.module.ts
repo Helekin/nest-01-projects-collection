9cbdfed3f7c550f211f3c7af1ab23fa66effc1d2
Helekin <clsjos2294@gmail.com>
2026-02-16 15:20:01 -0500

    Pokedex

--- nest-03-pokedex/src/app.module.ts
+++ nest-03-pokedex/src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
+
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@localhost:27017/${process.env.MONGO_DB}?authSource=admin`,
+    ),
     PokemonModule,
+    CommonModule,
   ],
 })
 export class AppModule {}
