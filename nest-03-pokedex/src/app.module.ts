b05a713c819892ac1b0f82c4c904653dc5e6901f
Helekin <clsjos2294@gmail.com>
2026-02-23 21:00:49 -0500

    UPDATE: pokemon

--- nest-03-pokedex/src/app.module.ts
+++ nest-03-pokedex/src/app.module.ts
@@ -6,6 +6,7 @@ import { join } from 'path';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { CommonModule } from './common/common.module';
     ),
     PokemonModule,
     CommonModule,
+    SeedModule,
   ],
 })
 export class AppModule {}
